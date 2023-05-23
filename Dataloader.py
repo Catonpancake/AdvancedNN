04c01c4d81c0739cbc7de92d9267ffd32e0d5e47
unknown <rantoringagain@gmail.com>
2023-05-23 16:44:22 +0900

    working on test

--- Dataloader.py
+++ Dataloader.py
@@ -1,5 +1,6 @@
 import os
 import torch
+import random
 import numpy as np
 import pandas as pd
 from torch.utils.data import Dataset
@@ -7,6 +8,8 @@ from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 from torchvision import datasets, transforms, models
 import torchvision.transforms.functional as F
+from torch.utils.data.sampler import SubsetRandomSampler
+from sklearn.model_selection import train_test_split
 
 class dataloader(Dataset):
     def __init__(self, img_dir, transform=None, test=False):
@@ -41,3 +44,23 @@ class SquarePad:
         p_right, p_bottom = [max_wh - (s+pad) for s, pad in zip(image.size, [p_left, p_top])]
         padding = (p_left, p_top, p_right, p_bottom)
         return F.pad(image, padding, 0, 'constant')
+    
+
+def load_split_train_vaild(datadir, transforms, valid_size = .2, batch_size=4, seed=42):
+    setseed(seed)
+    dataset = datasets.ImageFolder(datadir,       
+                    transform=transforms)
+    num_train = int(len(dataset)*(1-valid_size))
+    num_valid = len(dataset) - num_train
+    print(num_train, num_valid, num_train+num_valid, len(dataset))
+    traindata, validdata = train_test_split(dataset, test_size=valid_size, random_state=123, shuffle=True)
+    trainloader = DataLoader(traindata, batch_size=batch_size, num_workers=0, shuffle=True)
+    validloader = DataLoader(validdata, batch_size=batch_size, num_workers=0, shuffle=False)
+
+    return trainloader, validloader
+
+def setseed(seednum = 42):
+    random.seed(seednum)
+    np.random.seed(seednum)
+    torch.manual_seed(seednum)
+    torch.cuda.manual_seed_all(seednum)
--- utils_traintest.py
+++ utils_traintest.py
@@ -1,7 +1,7 @@
 import os
 import csv
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
-
+import pandas as pd
 
 import torch
 from torch import nn
@@ -11,30 +11,40 @@ from torch.utils.data import DataLoader
 import utils as ut
 
 
-def train(dataloader: DataLoader, model, loss_fn, optimizer, device):
+def train(dataloader: DataLoader, model, loss_fn, optimizer, device, epoch, seed):
+    model.to(device)
     size = len(dataloader.dataset)
+    df = pd.DataFrame()
+    correct = 0
     for batch, (X, y) in enumerate(dataloader):
         X, y = X.to(device).float(), y.to(device)
 
         # 예측 오류 계산
         pred = model(X)
         loss = loss_fn(pred, y)
+        correct += (pred.argmax(1) == y).type(torch.float).sum().item()
 
         # 역전파
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+    
 
         if batch % 100 == 0:
             loss, current = loss.item(), batch * len(X)
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+    current = {'loss': loss.cpu().detach().numpy()/len(dataloader), 'accuracy': (correct/size)*100, 'lable': 'train', 'epoch': epoch, 'seed': seed}
+    df = df.append(current, ignore_index = True)
+    return df
             
-def test(dataloader, model, loss_fn, device, valid=True):
-    
+def test(dataloader, model, loss_fn, device, epoch, seed, valid=True):
+    model.to(device)
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
     model.eval()
     test_loss, correct = 0, 0
+    df = pd.DataFrame()
     with torch.no_grad():
         if valid:
             for X, y in dataloader:
@@ -45,6 +55,8 @@ def test(dataloader, model, loss_fn, device, valid=True):
             test_loss /= num_batches
             correct /= size
             print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+            current = {'loss': test_loss, 'accuracy': correct*100, 'lable': 'valid', 'epoch': epoch, 'seed': seed}
+            df = df.append(current, ignore_index = True)
         elif valid == False:
             counter = 0
             filename = f"Submissions/Submission_{counter}.csv"
@@ -53,9 +65,11 @@ def test(dataloader, model, loss_fn, device, valid=True):
                 filename = f"Submissions/Submission_{counter}.csv"
             with open(filename, 'a+', newline='') as f:  # 'a+' means append to a file
                 thewriter = csv.writer(f)
-                for X in dataloader:
+                for X, y in dataloader:
                     X = X.to(device).float()
                     pred = model(X)
                     result = pred.argmax().item()  # this your label
                     print(result)
                     thewriter.writerow([result])
+    
+    return df
